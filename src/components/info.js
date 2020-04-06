e538aa94ce0832f4877f6c69ebc97958edceb723
Gene Smith <me@genesmith.org>
2020-04-06 17:11:56 -0400

    initial commit

--- src/components/info.js
+++ src/components/info.js
@@ -3,9 +3,14 @@ import React from "react"
 import {
   FaTwitter as Twitter,
   FaGithub as Github,
-  FaYoutube as Youtube,
-  FaInstagram as Insta,
-  FaEnvelope as Mail,
+  FaLinkedinIn as LinkedIn,
+  FaAngellist as AngelList,
+  FaFacebookF as Facebook,
+  FaRedditAlien as Reddit,
+  FaPinterest as Pinterest,
+  FaSpotify as Spotify,
+  FaSoundcloud as Soundcloud,
+  FaMediumM as Medium
 } from "react-icons/fa"
 
 export default function info() {
@@ -13,37 +18,46 @@ export default function info() {
     <div className="container">
       <div className="my-5"></div>
       <div className="display-3" style={{ color: "#FF8A00" }}>
-        <span style = {{color : "white"}}>Hello, I'm</span> <br />
-        Sumanth.
+        <span style={{ color: "white" }}>Hello, I'm</span> <br />
+        Gene Smith.
       </div>
       <div className="h1 code mt-4 mb-3">async {"{"}</div>
-      <div className="text-muted mx-5 my-4 h3 text-justify info">
-        A Passionate Front-end Developer, UI/UX Enthusiast, Android Geek and Web
-        Developer.
-        <br /> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et.
+      <div className="text-muted mx-5 my-4 h3 info">
+        Seasoned marketing executive, experienced marketing technologist and a machine learning & blockchain enthusiast. Follow me. Contact me. Etc....
       </div>
       <div className="h1 code mt-2 mb-3">{"}"}</div>
       <div className="h1 mt-5">
-        <a className="mr-5 icon" href="https://twitter.com/Mr404Found">
-          <Twitter />
+        <a className="mr-5 icon" href="https://www.linkedin.com/in/genesmith1" target="_blank" rel="noopener noreferrer">
+          <LinkedIn />
         </a>
-        <a className="mr-5 icon" href="https://instagram.com/sumanth.zero7">
-          <Insta />
+        <a className="mr-5 icon" href="https://angel.co/gene-smith?public_profile=1" target="_blank" rel="noopener noreferrer">
+          <AngelList />
         </a>
-        <a className="mr-5 icon" href="https://github.com/Mr404Found">
+        <a className="mr-5 icon" href="https://www.facebook.com/gene.r.smith" target="_blank" rel="noopener noreferrer">
+          <Facebook />
+        </a>
+        <a className="mr-5 icon" href="https://github.com/gene-smith" target="_blank" rel="noopener noreferrer">
           <Github />
         </a>
-        <a
-          className="mr-5 icon"
-          href="https://www.youtube.com/channel/UCw0FDIL-HpcfzEZrjcQwhMQ"
-        >
-          <Youtube />
+        <a className="mr-5 icon" href="https://www.reddit.com/user/gene-smith/" target="_blank" rel="noopener noreferrer">
+          <Reddit />
         </a>
-        <a className="mr-5 icon" href="mailto://contactmr404here@gmail.com">
-          <Mail />
+        <a className="mr-5 icon" href="https://twitter.com/gene_r_smith" target="_blank" rel="noopener noreferrer">
+          <Twitter />
+        </a>
+        <a className="mr-5 icon" href="https://medium.com/@gene_r_smith" target="_blank" rel="noopener noreferrer">
+          <Medium />
+        </a>
+        <a className="mr-5 icon" href="http://www.pinterest.com/gene_smith.com/gene-smith" target="_blank" rel="noopener noreferrer">
+          <Pinterest />
+        </a>
+        <a className="mr-5 icon" href="https://open.spotify.com/user/genesmith?si=xDf3RF02RFCHuXH5FZB82w" target="_blank" rel="noopener noreferrer">
+          <Spotify />
+        </a>
+        <a className="mr-5 icon" href="https://soundcloud.com/gene_smith" target="_blank" rel="noopener noreferrer">
+          <Soundcloud />
         </a>
       </div>
-    </div>
+    </div >
   )
 }
