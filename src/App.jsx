713ec4ea8cef89effe2e3f9e405c3d8607303761
Gene Smith <me@genesmith.org>
2025-07-24 14:52:23 -0400

    Add dark background styles for proper dark mode display

--- src/App.jsx
+++ src/App.jsx
@@ -47,6 +47,7 @@ function App() {
 
   return (
     <motion.div
+      className='min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-white'
       initial='initial'
       animate='in'
       exit='out'
